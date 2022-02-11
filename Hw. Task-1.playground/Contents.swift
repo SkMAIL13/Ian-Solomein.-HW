80b2ef2bebcd74246cd5c9dc766c8230c95e5b8e
Kamran <iskama98@mail.ru>
2022-02-11 14:54:34 +0300

    feat: complete Task-1

--- Hw. Task-1.playground/Contents.swift
+++ Hw. Task-1.playground/Contents.swift
@@ -3,3 +3,29 @@ import UIKit
 /*
         1 Task. Ian Solomein
 */
+
+// Проверка символов в строке на уникальность
+
+// Задание: написать ф-ю, которая принимает строку как свой единственный параметр и возвращает true, если строка содержит только уникальные сиволы
+
+//===========================================================================================================================
+
+// Решение
+
+
+func uniqueSymbols(input: String) -> Bool {
+    
+    var checkedSymbols = [Character]()
+
+    for symbol in input {
+        if checkedSymbols.contains(symbol) {
+            return false
+        }
+        checkedSymbols.append(symbol)
+    }
+
+    return true
+}
+
+//uniqueSymbols(input: "Homework")
+//uniqueSymbols(input: "1")
