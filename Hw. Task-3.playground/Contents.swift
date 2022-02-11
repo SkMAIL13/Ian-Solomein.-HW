e9a637b6f8d5fd995daecc3a4b275d95c8168b67
Kamran <iskama98@mail.ru>
2022-02-11 17:11:34 +0300

    feat: complete Task-3

--- Hw. Task-3.playground/Contents.swift
+++ Hw. Task-3.playground/Contents.swift
@@ -4,10 +4,18 @@ import UIKit
         3 Task. Ian Solomein
 */
 
-// Является ли строка пангрмаммой?
+// Идентичны ли две строки между собой?
 
 /* Задание: Написать ф-ю, которая принимает две строки и возвращает true, если эти строки содержат одни и те же символы, в любом порядке с учетом регистра.*/
 
 //===========================================================================================================================
 
 // Решение
+
+func isSameSymbols (input1: String, input2: String) -> Bool {
+    return input1.sorted() == input2.sorted()
+}
+
+isSameSymbols(input1:  "TEST", input2: "TEST")
+isSameSymbols(input1: "tEst", input2: "TEST")
+isSameSymbols(input1: "test", input2: "tset")
