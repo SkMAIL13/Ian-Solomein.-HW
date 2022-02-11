deb5d4d855838915a6ac16fafd340246e4b5807b
Kamran <iskama98@mail.ru>
2022-02-11 17:54:52 +0300

    feat: complete Task-4

--- Hw. Task-4.playground/Contents.swift
+++ Hw. Task-4.playground/Contents.swift
@@ -1,5 +1,7 @@
 import UIKit
 
+import Foundation
+
 /*
         4 Task. Ian Solomein
 */
@@ -11,3 +13,16 @@ import UIKit
 //===========================================================================================================================
 
 // Решение
+
+
+
+func isContain(string1: String, string2: String) -> Bool {
+    
+    if string2.lowercased() == string1.lowercased() {
+        return true
+    }
+    return false
+}
+
+isContain(string1: "test", string2: "test")
+isContain(string1: "1", string2: "2")
